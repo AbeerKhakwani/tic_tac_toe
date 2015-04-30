53e4bba7622ef9f8e46d4f711b9b8cac1b2048b4
David Eliason & Abeer Khakwani <student@epicodus.com>
2015-04-30 14:03:53 -0700

    git add function for checking row and display winning message

--- app.js
+++ app.js
@@ -19,15 +19,40 @@ tictactoe.controller("TicTacToeCtrl", function TicTacToeCtrl($scope){
         });
     };
     $scope.reset();
+    var checkForEndOfGame =function(){
+
+    }
+
+    var checkRow = function(){
+        var current = false;
+        for (var i = 0; i< $scope.board.length; i++){
+                for (var x = 0; x < $scope.board[i].length; x++){
+                    console.log($scope.board[i][x].value);
+                if($scope.board[i][0].value === $scope.board[i][1].value
+                    && $scope.board[i][0].value === $scope.board[i][2].value
+                    && $scope.board[i][0].value !== '-')
+                    {
+                     current = true;
+
+                    }
+            }
+        }
+        return current;
+    };
 
     $scope.move= function(cell){
         cell.value = $scope.currentPlayer;
-        if ($scope.currentPlayer === "x"){
-            $scope.currentPlayer = "o";
-        }
-        else {
-            $scope.currentPlayer = "x";
-        }
+
+         if(checkRow() == false ){
+                if ($scope.currentPlayer === "x"){
+                    $scope.currentPlayer = "o";
+                }
+                else {
+                    $scope.currentPlayer = "x";
+                }
+         }else{
+             console.log("match! Game Over!!!! ");
+         }
     };
 
 });
@@ -35,7 +60,7 @@ tictactoe.controller("TicTacToeCtrl", function TicTacToeCtrl($scope){
 tictactoe.directive("reset", function(){
     return function(scope,element){
             element.bind("click",function(){
-             element.after("<div class=''>New game!</div>")
+             element.after("<div class=''>New game!</div>");
          })
     }
 });
