300fb79884949405cf08814c9d8ef4a82f40f4fa
David Eliason & Abeer Khakwani <student@epicodus.com>
2015-04-30 11:45:24 -0700

    add function to change tile to player's sign

--- app.js
+++ app.js
@@ -9,15 +9,27 @@ tictactoe.controller("TicTacToeCtrl", function TicTacToeCtrl($scope){
     ];
 
     $scope.reset = function(){
-        angular.forEach($scope.board, function(value,key){
+      angular.forEach($scope.board, function(value,key){
             angular.forEach(value, function(cell,keyOfCell){
                        cell.value='-';
             });
             $scope.currentPlayer="x";
-            console.log($scope.currentPlayer);
+            $scope.winner="No Winner";
+            console.log($scope.winner);
         });
+    };
+    $scope.reset();
 
+    $scope.move= function(cell){
+        cell.value = $scope.currentPlayer;
+    };
 
+});
 
+tictactoe.directive("reset", function(){
+    return function(scope,element){
+            element.bind("click",function(){
+             element.after("<div class=''>New game!</div>")
+         })
     }
 });
